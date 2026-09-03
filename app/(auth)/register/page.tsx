b57ce12c5ebbdcd20dea09a484c95da98a23f756
chinyex <obiakorchinenye47@gmail.com>
2026-09-03 10:42:56 +0100

    Fix auth layout

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,10 +1,9 @@
 import AuthLayout from "@/components/auth/AuthLayout";
-import AuthPreview from "@/components/auth/AuthPreview";
 import RegisterForm from "@/components/auth/RegisterForm";
 
 export default function RegisterPage() {
   return (
-    <AuthLayout preview={<AuthPreview />}>
+    <AuthLayout>
       <RegisterForm />
     </AuthLayout>
   );
