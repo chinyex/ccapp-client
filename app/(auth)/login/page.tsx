64440c9adcfa974287b24f3d2ca9d70686ec8cdd
chinyex <obiakorchinenye47@gmail.com>
2026-09-03 09:59:04 +0100

    update project

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,9 @@
 import AuthLayout from "@/components/auth/AuthLayout";
-import AuthPreview from "@/components/auth/AuthPreview";
 import LoginForm from "@/components/auth/LoginForm";
 
 export default function LoginPage() {
   return (
-    <AuthLayout preview={<AuthPreview />}>
+    <AuthLayout>
       <LoginForm />
     </AuthLayout>
   );
